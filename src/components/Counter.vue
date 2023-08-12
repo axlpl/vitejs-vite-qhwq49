fce5e8a03588f281d6b3bf41132a9b7aba32c92b
axlpl <krystian.maslo@gmail.com>
2023-08-12 23:57:52 +0200

    2 files modified, 1 added, 1 deleted

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,17 +1,12 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import Counter from './components/Counter.vue';
 
 const value = ref<number>(0);
 </script>
 
 <template>
-  <Counter v-model="value" />
-  <hr />
   <div class="content">
     <button @click="value += 1">+</button>
     <h2>{{ value }}</h2>
   </div>
-  <hr />
-  <input v-model.number="value" />
 </template>
