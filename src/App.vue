bc76e364bf754a9be44ce8492614bc44ddb3b2d1
axlpl <krystian.maslo@gmail.com>
2023-08-13 00:13:32 +0200

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
 import { ref } from 'vue';
 import Counter from './components/Counter.vue';
 
+// v-model, which means two-way binding.
+// Do not modify the App.js file.
+// We must enable v-model between App and Counter.
+
 const value = ref<number>(0);
 </script>
 
